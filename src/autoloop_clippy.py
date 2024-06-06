bcd385d2c7640ad6ae4c5cfb9041b407fbcdf05a
Praful Patekar <prafulpatekar23@gmail.com>
2024-06-06 14:19:27 +0530

    [Commit: 5] Adding Source code

---
+++ src/autoloop_clippy.py
@@ -0,0 +1,37 @@
+from tkinter import Tk
+import pyperclip
+
+import src.clipbot as clipbot
+
+class TestClippyVisual():
+
+    def __init__(self):
+        self.iterVal = 0
+        self.debug = True
+
+        self.root = Tk()
+        self.ClippyTestClass = clipbot.Clippy(self.root)
+        self.ClippyTestClass.maxClippingsOnApp = 5
+        self.ClippyTestClass.createLayout()
+        self.testClippyVisual()
+        self.ClippyTestClass.updateClipboard()
+        self.ClippyTestClass.mainloop()
+
+    def testClippyVisual(self):
+        cliptext = "cliptext-" + str(self.iterVal)
+        self.iterVal += 1
+        pyperclip.copy(cliptext)
+        print("sent to clipbot -> ", cliptext)
+        if self.debug:
+            print("*"*100)
+            print("clipboardContent -> ", self.ClippyTestClass.clipboardContent)
+            print()
+            print("clipboardContentMapping -> ", self.ClippyTestClass.clipboardContentMapping)
+            print()
+            print("labelArray :")
+            [print(list(labelElem.items())) for labelElem in self.ClippyTestClass.labelArray]
+            print("*"*100)
+        self.root.after(ms=1500, func=self.testClippyVisual)
+
+if __name__ == '__main__':
+    TestClippyVisual()
